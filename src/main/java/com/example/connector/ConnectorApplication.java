b0a3561d2a50615fbd7fb0deb2747199a6b3bb31
Noah Chen <nchen@hamco.local>
2025-06-13 12:17:29 -0400

    refactored main to use standard practice.

--- src/main/java/com/example/connector/ConnectorApplication.java
+++ src/main/java/com/example/connector/ConnectorApplication.java
@@ -1,36 +1,46 @@
 package com.example.connector;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import com.example.connector.dto.TokenResponse;
+import com.example.connector.dto.TokenResponseDto;
 import com.example.connector.netsuite.NetsuiteAuthClient;
 import com.example.connector.netsuite.NetsuiteCustomerClient;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @SpringBootApplication
-public class ConnectorApplication {
+public class ConnectorApplication implements CommandLineRunner {
+	@Autowired
+	private NetsuiteAuthClient netsuiteAuthClient;
+
+	@Autowired
+	private NetsuiteCustomerClient netsuiteCustomerClient;
+
+	private String accessToken;
+	private long expiresIn;
+
+	@Override
+	public void run(String... args) throws Exception {
+		try {
+			String tokenRes = netsuiteAuthClient.fetchAccessToken();
+			ObjectMapper mapper = new ObjectMapper();
+			TokenResponseDto parsedRes = mapper.readValue(tokenRes, TokenResponseDto.class);
+			accessToken = parsedRes.getAccess_token();
+			expiresIn = System.currentTimeMillis() + parsedRes.getExpires_in();
+			// String custResponse = netsuiteCustomerClient.getCustomerEmail(token, "149777");
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(ConnectorApplication.class, args);
 	}
-
-	@Bean
-	public CommandLineRunner run(NetsuiteAuthClient netsuiteAuthClient, NetsuiteCustomerClient netsuiteCustomerClient) {
-		return args -> {
-			try {
-				String tokenResponse = netsuiteAuthClient.fetchAccessToken();
-				ObjectMapper mapper = new ObjectMapper();
-				TokenResponse response = mapper.readValue(tokenResponse, TokenResponse.class);
-				String token = response.getAccess_token();
-				String custResponse = netsuiteCustomerClient.getCustomerEmail(token, "149777");
-				
-				System.out.println("res " + custResponse);
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		};
-	}
 }
+
+
+
--- src/main/java/com/example/connector/dto/TokenResponseDto.java
+++ src/main/java/com/example/connector/dto/TokenResponseDto.java
@@ -1,6 +1,6 @@
 package com.example.connector.dto;
 
-public class TokenResponse {
+public class TokenResponseDto {
     private String access_token;
     private String token_type;
     private int expires_in;
